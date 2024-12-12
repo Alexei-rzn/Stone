fe7249d38789aecdd06fd62aee559c7e3f737e26
Alexei-rzn <onlinepeople24@gmail.com>
2024-12-12 21:12:33 +0300

    Update get_users.php

--- get_users.php
+++ get_users.php
@@ -14,13 +14,13 @@ if ($conn->connect_error) {
 }
 
 // Выполняем SQL-запрос
-$sql = "SELECT id, name, email FROM users";
+$sql = "SELECT ШК 1, ШК 2, ШК 3";
 $result = $conn->query($sql);
 
 // Проверяем, есть ли результаты и выводим их
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+        echo "ШК 1: " . $row["ШК 1"]. " - ШК 2: " . $row["ШК 2"]. " - ШК 3: " . $row["ШК 3"]. "<br>";
     }
 } else {
     echo "0 results";
